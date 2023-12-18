e0b473f0150c3a94f74df34d0e29d4bc14012d1d
amoghkokari <amoghsrcm@gmail.com>
2023-12-17 21:24:48 -0500

    api link button

--- stremlit_llm.py
+++ stremlit_llm.py
@@ -8,7 +8,7 @@ import google.generativeai as genai
 st.title('🦜 Get Food Recipe')
 st.subheader('Get Food Recipe based on Grocery, Time Required, Cuisines and Equipment avaialable (all inputs are optional)')
 api_key =  st.text_input('Enter Google Generative AI API KEY (Required)')
-st.text("Api key -> https://makersuite.google.com/app/apikey (select create api key in new project)")
+st.link_button("Click for Api key and (select create api key in new project)", "https://makersuite.google.com/app/apikey", type="secondary")
 food = st.text_input('Particular Food in Mind (Dal Tadka)')
 grocery = st.text_input('Grocery (onion, garam masala)')
 time = st.text_input('Cooking Time (1 hr, 30 mins)')
